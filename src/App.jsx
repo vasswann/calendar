55b5fa22a9fa7c684b6f83ff2014bdd777cda9bd
Istvan Vas <swannxxx@yahoo.co.uk>
2022-08-15 00:42:24 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Calendar from './component/Calendar';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='header-text'>Calendar </h1>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
